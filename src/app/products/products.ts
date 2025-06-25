66a74bda5a3f226c6c02df4f0908dc005851590c
BARNOCH Anas <anasbarnoch@gmail.com>
2025-06-25 23:04:50 +0100

    products (data bindings && handle delete product

--- src/app/products/products.ts
+++ src/app/products/products.ts
@@ -1,11 +1,29 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+// import {NgForOf} from '@angular/common';
 
 @Component({
   selector: 'app-products',
-  imports: [],
+  imports: [
+    // NgForOf
+  ],
   templateUrl: './products.html',
   styleUrl: './products.css'
 })
-export class ProductsComponent {
+export class ProductsComponent implements OnInit {
+  products: Array<any> = [];
 
+  constructor() {
+  }
+
+  ngOnInit(): void {
+    this.products = [
+      {id: 1, name : "Computer", price : 6000, description : "Dell computer", promo: false},
+      {id: 2, name : "Smart phone", price : 3000, description : "Iphone", promo: true},
+      {id: 3, name : "Printer", price : 8000, description : "Wireless Printer",promo: false},
+    ];
+  }
+
+  handleDelete(id: number) {
+    this.products = this.products.filter(product => product.id !== id);
+  }
 }
