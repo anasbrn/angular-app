bca80943c5c86f7e4d7ceeb33d70e1502eb0864f
BARNOCH Anas <anasbarnoch@gmail.com>
2025-06-25 21:58:55 +0100

    Add routes & home, products components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import {ProductsComponent} from './products/products';
+import {HomeComponent} from './home/home';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path: "home", component: HomeComponent},
+  {path:"products", component: ProductsComponent}
+];
